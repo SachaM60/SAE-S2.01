a631cff5d233000100327d6cc908b327e1a16988
Tarik <tarikbenrabah@outlook.com>
2025-05-30 22:47:51 +0200

    Début de configuration de la page Trajets Favoris, je n'ai pas encore implémenté la BD dedans.

--- PageTrajetsFavoris.Designer.cs
+++ PageTrajetsFavoris.Designer.cs
@@ -32,13 +32,18 @@
             btnCalculItinéraire = new Button();
             btnFavoris = new Button();
             btnMenu = new Button();
+            lblDispo = new Label();
+            lblFavs = new Label();
+            listDispo = new ListBox();
+            listFav = new ListBox();
             SuspendLayout();
             // 
             // btnPlan
             // 
-            btnPlan.Location = new Point(600, 421);
+            btnPlan.Location = new Point(750, 526);
+            btnPlan.Margin = new Padding(4);
             btnPlan.Name = "btnPlan";
-            btnPlan.Size = new Size(201, 29);
+            btnPlan.Size = new Size(251, 36);
             btnPlan.TabIndex = 8;
             btnPlan.Text = "Plan du réseau";
             btnPlan.UseVisualStyleBackColor = true;
@@ -46,9 +51,10 @@
             // 
             // btnCalculItinéraire
             // 
-            btnCalculItinéraire.Location = new Point(383, 421);
+            btnCalculItinéraire.Location = new Point(479, 526);
+            btnCalculItinéraire.Margin = new Padding(4);
             btnCalculItinéraire.Name = "btnCalculItinéraire";
-            btnCalculItinéraire.Size = new Size(220, 29);
+            btnCalculItinéraire.Size = new Size(275, 36);
             btnCalculItinéraire.TabIndex = 7;
             btnCalculItinéraire.Text = "Calcul d'itinéraire";
             btnCalculItinéraire.UseVisualStyleBackColor = true;
@@ -57,9 +63,10 @@
             // btnFavoris
             // 
             btnFavoris.BackColor = SystemColors.Highlight;
-            btnFavoris.Location = new Point(192, 421);
+            btnFavoris.Location = new Point(240, 526);
+            btnFavoris.Margin = new Padding(4);
             btnFavoris.Name = "btnFavoris";
-            btnFavoris.Size = new Size(194, 29);
+            btnFavoris.Size = new Size(242, 36);
             btnFavoris.TabIndex = 6;
             btnFavoris.Text = "Trajets favoris";
             btnFavoris.UseVisualStyleBackColor = false;
@@ -67,26 +74,70 @@
             // btnMenu
             // 
             btnMenu.BackColor = Color.White;
-            btnMenu.Location = new Point(-1, 421);
+            btnMenu.Location = new Point(-1, 526);
+            btnMenu.Margin = new Padding(4);
             btnMenu.Name = "btnMenu";
-            btnMenu.Size = new Size(197, 29);
+            btnMenu.Size = new Size(246, 36);
             btnMenu.TabIndex = 5;
             btnMenu.Text = "Menu";
             btnMenu.UseVisualStyleBackColor = false;
             btnMenu.Click += btnMenu_Click;
             // 
+            // lblDispo
+            // 
+            lblDispo.AutoSize = true;
+            lblDispo.Location = new Point(78, 51);
+            lblDispo.Name = "lblDispo";
+            lblDispo.Size = new Size(158, 25);
+            lblDispo.TabIndex = 9;
+            lblDispo.Text = "Lignes disponibles";
+            // 
+            // lblFavs
+            // 
+            lblFavs.AutoSize = true;
+            lblFavs.Location = new Point(714, 51);
+            lblFavs.Name = "lblFavs";
+            lblFavs.Size = new Size(135, 25);
+            lblFavs.TabIndex = 10;
+            lblFavs.Text = "Lignes favorites";
+            lblFavs.Click += label2_Click;
+            // 
+            // listDispo
+            // 
+            listDispo.FormattingEnabled = true;
+            listDispo.ItemHeight = 25;
+            listDispo.Location = new Point(78, 97);
+            listDispo.Name = "listDispo";
+            listDispo.Size = new Size(180, 379);
+            listDispo.TabIndex = 11;
+            // 
+            // listFav
+            // 
+            listFav.FormattingEnabled = true;
+            listFav.ItemHeight = 25;
+            listFav.Location = new Point(714, 97);
+            listFav.Name = "listFav";
+            listFav.Size = new Size(180, 379);
+            listFav.TabIndex = 12;
+            // 
             // PageTrajetsFavoris
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1000, 562);
+            Controls.Add(listFav);
+            Controls.Add(listDispo);
+            Controls.Add(lblFavs);
+            Controls.Add(lblDispo);
             Controls.Add(btnPlan);
             Controls.Add(btnCalculItinéraire);
             Controls.Add(btnFavoris);
             Controls.Add(btnMenu);
+            Margin = new Padding(4);
             Name = "PageTrajetsFavoris";
             Text = "Trajets favoris";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -95,5 +146,9 @@
         private Button btnCalculItinéraire;
         private Button btnFavoris;
         private Button btnMenu;
+        private Label lblDispo;
+        private Label lblFavs;
+        private ListBox listDispo;
+        private ListBox listFav;
     }
 }
--- PageTrajetsFavoris.cs
+++ PageTrajetsFavoris.cs
@@ -15,8 +15,51 @@ namespace SAE_S2._01
         public PageTrajetsFavoris()
         {
             InitializeComponent();
+            ConfigListes();
         }
 
+        private void ConfigListes()
+        {
+            // BD non utilisée
+            listDispo.Items.AddRange(new string[] {
+                "Ligne 9",
+                "Ligne 10",
+                "Ligne 11",
+                "Ligne 12",
+                "Ligne 13",
+                "Ligne 14",
+                "Ligne 15",
+                "Ligne 16",
+                "Ligne 17",
+                "Ligne 18",
+                "Ligne 19",
+                "Ligne 20",
+                "Ligne 21",
+                "Ligne 22",
+                "Ligne 11 Express",
+            });
+
+            listDispo.MouseDoubleClick += DispoToFav;
+            listFav.MouseDoubleClick += FavToDispo;
+        }
+
+        private void FavToDispo(object sender, EventArgs e)
+        {
+            if (listFav.SelectedItem != null)
+            {
+                listDispo.Items.Add(listFav.SelectedItem);
+                listFav.Items.Remove(listFav.SelectedItem);
+            }
+        }
+
+        private void DispoToFav(object sender, MouseEventArgs e)
+        {
+            if (listDispo.SelectedItem != null)
+            {
+                listFav.Items.Add(listDispo.SelectedItem);
+                listDispo.Items.Remove(listDispo.SelectedItem);
+            }
+        }
         private void btnMenu_Click(object sender, EventArgs e)
         {
             PageMenuPrincipal pageMenuPrincipal = new PageMenuPrincipal();
@@ -36,6 +79,11 @@ namespace SAE_S2._01
             PagePlanDuReseau pagePlanDuReseau = new PagePlanDuReseau();
             pagePlanDuReseau.Show();
             this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
